1bc763efba6a1a77b26a9a40818f58a91924cbb2
Khang Vu <kmvu91@gmail.com>
2019-02-22 21:04:11 +0700

    feat: add Actor (Policy) model

---
+++ models/actor.py
@@ -0,0 +1,69 @@
+from keras import layers, models, optimizers
+from keras import backend as K
+from keras.layers import Input, Dense, Lambda
+
+
+"""Policy model"""
+class Actor:
+
+	def __init__(self, state_size, action_size, action_dim_min, action_dim_max):
+		"""Initialize parameters and build model
+		Params
+		======
+			state_size (int): Dimension of each state
+			action_size (int): Dimension of each action
+			action_dim_min (int): Min value for each action dimension
+			action_dim_max (int): Max value for each action dimension
+		"""
+		self.state_size = state_size
+		self.action_size = action_size
+		self.action_dim_min = action_dim_min
+		self.action_dim_max = action_dim_max
+		self.action_range = self.action_dim_max - self.action_dim_min
+
+		self.build_model()
+
+	def build_model(self):
+		"""Build an Actor (Policy) network that maps states -> actions"""
+
+		# Input layer (states)
+		states = Input((self.state_size,), name = 'states')
+
+		# Hidden layers
+		net = Dense(512, activation = 'relu')(states)
+		net = Dense(128, activation = 'relu')(net)
+		net = Dense(32, activation = 'relu')(net)
+
+		# Final output layer with sigmoide layer
+		raw_actions = Dense(self.action_size, activation = 'relu', name = 'raw_actions')(net)
+
+		# Scale [0, 1] output for each action dimension to proper range
+		actions = Lambda(lambda action: action * self.action_range + self.action_dim_min, 
+						 name = 'actions')(raw_actions)
+
+		# Create our Keras model
+		self.model = models.Model(inputs = states, output = actions)
+
+		# Define loss function using action value (Q value) gradients
+		action_gradients = Input((self.action_size,))
+		loss = K.mean(-action_gradients * actions)
+
+		# More losses here (e.g. from regulizers)
+		# ...
+
+
+		# Define optimizers and training function
+		optimizer = optimizers.Adam()
+		update_op = optimizer.get_updates(self.model.trainable_weights, loss = loss)
+
+		self.train_func = K.function(
+			inputs = [self.model.input, action_gradients, K.learning_phase()],
+			outpus = [],
+			updates = updates_op
+		)
+
+
+
+
+
+
